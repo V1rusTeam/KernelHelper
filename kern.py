c4bd1cbf9eebe31ff08eb5f8c9180f3f47fa860a
V1rusTeam <62395719+V1rusTeam@users.noreply.github.com>
2020-03-24 10:00:11 +0000

    Update

--- kern.py
+++ kern.py
@@ -12,7 +12,6 @@ def ker():
     os.system("git clone https://github.com/lineage-x2-devs/android_kernel_leeco_msm8996/ -b lineage-16.0 kernel_lineageos-16_leeco")
     os.system("wget https://releases.linaro.org/components/toolchain/binaries/4.9-2017.01/aarch64-linux-gnu/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu.tar.xz")
     os.system("tar xf gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu.tar.xz")
-    os.system()
 
 
 ui = input("хуярим?(y/n): ")
