0396c64c987b66dccafa840bebb61677db79a26c
V1rusTeam <62395719+V1rusTeam@users.noreply.github.com>
2020-03-26 11:36:44 +0700

    Update TestesForScript.py

--- TestesForScript.py
+++ TestesForScript.py
@@ -4,14 +4,23 @@ from time import sleep
 sleep(2)
 
 a = pyautogui.locateCenterOnScreen("File.png")
-b = pyautogui.locateCenterOnScreen("clse.png")
 
 if a == None:
     print("[-] File not found")
 else:
     print("[+] File found!")
 
+if a != None:
+    x, y = pyautogui.locateCenterOnScreen("File.png")
+    pyautogui.doubleClick(x, y)
+
+b = pyautogui.locateCenterOnScreen("clse.png")
+
 if b == None:
     print("[-] Close not found")
 else:
     print("[+] Close found!")
+
+if b != None:
+    x, y = pyautogui.locateCenterOnScreen("clse.png")
+    pyautogui.click(x, y)
